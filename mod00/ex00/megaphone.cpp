3d30ad42692edaaf70e1d485b2e23131475ea92a
luna <luna7111@tuta.io>
2025-07-04 12:15:59 +0200

    Added ex00 source file

---
+++ mod00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+//                                          _.._  .           .
+//  megaphone.cpp                         .' .-'`        *
+//                                       /  /       +        *
+//  by: luna del Valle                   |  |           *
+//                                       \  '.___.;       +
+//  Creative Commons CC0 1.0 Universal    '._  _.'   .        .
+//                                           ``
+
+#include <iostream>
+
+std::string  capitalize_string(char* str)
+{
+    std::string result = str;
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        result[i] = std::toupper(str[i]);
+    }
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    int index = 1;
+
+    if (argc > 1) {
+        while (index < argc) {
+            std::cout << capitalize_string(argv[index]);
+            index ++;
+        }
+        std::cout << std::endl;
+        return (0);
+    }
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
