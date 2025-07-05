7e62107619c948ad0db45037cf14a30f167f493c
luna <luna7111@tuta.io>
2025-07-05 21:30:22 +0200

    Added some ~style~ :p also fixed a leak, im still figuring classes out hehe

--- mod00/ex01/src/PhoneBook.cpp
+++ mod00/ex01/src/PhoneBook.cpp
@@ -11,6 +11,10 @@
 #include <iostream>
 #include <sstream>
 
+#define YELLOW "\e[0;33m"
+#define WHITE "\e[0;37m"
+#define GREEN "\e[0;32m"
+
 PhoneBook::PhoneBook(void) {
 	contact_number = 0;
 }
@@ -22,7 +26,6 @@ PhoneBook::~PhoneBook(void) {
 }
 
 void	PhoneBook::cycle_contacts(void) {
-	contact[7].~Contact();
 	for (int i = 7; i > 0; i--) {
 		contact[i] = contact[i - 1];
 	}
@@ -35,15 +38,15 @@ void	PhoneBook::add_contact(void) {
 	std::string	phone_number;
 	std::string	darkest_secret;
 
-	std::cout<<"Enter first name:"<<std::endl;
+	std::cout<<YELLOW<<"Enter first name:"<<WHITE<<std::endl;
 	std::getline(std::cin, first_name);
-	std::cout<<"Enter last name:"<<std::endl;
+	std::cout<<YELLOW<<"Enter last name:"<<WHITE<<std::endl;
 	std::getline(std::cin, last_name);
-	std::cout<<"Enter nickname:"<<std::endl;
+	std::cout<<YELLOW<<"Enter nickname:"<<WHITE<<std::endl;
 	std::getline(std::cin, nickname);
-	std::cout<<"Enter phone number:"<<std::endl;
+	std::cout<<YELLOW<<"Enter phone number:"<<WHITE<<std::endl;
 	std::getline(std::cin, phone_number);
-	std::cout<<"Enter their darkest secret:"<<std::endl;
+	std::cout<<YELLOW<<"Enter their darkest secret:"<<WHITE<<std::endl;
 	std::getline(std::cin, darkest_secret);
 	cycle_contacts();
 	contact[0] = Contact(first_name, last_name, nickname, phone_number, darkest_secret);
@@ -55,18 +58,25 @@ void	PhoneBook::search_contact(void) {
 	std::string	user_input;
 	int			index_number;
 
+	if (contact_number == 0) {
+		std::cout<<"Add at least one contact first."<<std::endl;
+		return;
+	}
+
+	std::cout<<GREEN<<"---------------------------------------------"<<std::endl;
 	for (int i = 0; i < contact_number; i++) {
 		contact[i].print_overview(i + 1);
 	}
+	std::cout<<"---------------------------------------------"<<std::endl;
 
-	std::cout<<"Which contact do you want to search?:"<<std::endl;
+	std::cout<<YELLOW<<"Which contact do you want to search?:"<<WHITE<<std::endl;
 	std::getline(std::cin, user_input);
 	std::stringstream index_stream(user_input);
 	index_stream >> index_number;
 
 	while (index_stream.fail() || index_number < 1 || index_number > contact_number) {
 		index_stream.clear();
-		std::cout<<"Not a valid number. Try again:"<<std::endl;
+		std::cout<<YELLOW<<"Not a valid number. Try again:"<<WHITE<<std::endl;
 		index_stream.str(""); //may cause leakage??? idk
 		std::getline(std::cin, user_input);
 		std::stringstream index_stream(user_input);
--- mod00/ex01/src/main.cpp
+++ mod00/ex01/src/main.cpp
@@ -10,12 +10,15 @@
 #include <iostream>
 #include <string>
 
+#define PURPLE "\e[0;35m"
+#define WHITE "\e[0;37m"
+
 int	main(void) {
 	PhoneBook	phone_book;
 	std::string user_input;
 
 	while (1) {
-		std::cout<<"Enter command:"<<std::endl;
+		std::cout<<PURPLE<<"Enter command:"<<WHITE<<std::endl;
 		std::getline(std::cin, user_input);
 		if (user_input == "ADD" || user_input == "add")
 				phone_book.add_contact();
@@ -23,7 +26,6 @@ int	main(void) {
 				phone_book.search_contact();
 		else if (user_input == "EXIT" || user_input == "exit") {
 			std::cout<<"Goodbye!"<<std::endl;
-			phone_book.~PhoneBook();
 			return (0);
 		}
 	}
