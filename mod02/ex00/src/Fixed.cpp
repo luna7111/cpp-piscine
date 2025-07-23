98827962d714bbfdf11d075adc6e6dd582f5264b
luna <luna7111@tuta.io>
2025-07-23 14:22:17 +0200

    Added debug prints

--- mod02/ex00/src/Fixed.cpp
+++ mod02/ex00/src/Fixed.cpp
@@ -1,31 +1,48 @@
 /*
  * Name: Luna del Valle
  * File: Fixed.cpp
- * Created on: Sunday Jul 13, 2025 14:47:20 CEST
+ * Created on: Wednesday Jul 23, 2025 14:12:22 CEST
  */
 
 #include <Fixed.hpp>
+#include <iostream>
 
+//Default constructor
 Fixed::Fixed() {
-	this->int_value = 0;
+    std::cout<<"Default constructor called"<<std::endl;
+
+    this->int_value = 0;
 }
 
-Fixed::Fixed(const Fixed& copy) {
-	*this = copy;
+//Copy constructor
+Fixed::Fixed(const Fixed& source) {
+    std::cout<<"Copy constructor called"<<std::endl;
+
+    *this = source;
 }
 
+// Copy assignment operator
 Fixed& Fixed::operator=(const Fixed& rhs) {
-	this->int_value = rhs.int_value;
-	return (*this);
+    std::cout<<"Copy assignment operator called"<<std::endl;
+
+    this->int_value = rhs.getRawBits();
+
+    return (*this);
 }
 
+//Default destructor
 Fixed::~Fixed() {
+    std::cout<<"Destructor called"<<std::endl;
 }
 
 int Fixed::getRawBits(void) const {
+    std::cout<<"getRawBits member function called"<<std::endl;
+
 	return this->int_value;
 }
 
 void Fixed::setRawBits(int const raw) {
+    std::cout<<"setRawBits member function called"<<std::endl;
+
 	this->int_value = raw;
 }
--- mod02/ex00/src/main.cpp
+++ mod02/ex00/src/main.cpp
@@ -16,8 +16,8 @@ int main() {
     c = b;
 
     std::cout << a.getRawBits() << std::endl;
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
 
     return 0;
 }
