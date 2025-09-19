b2ce5169de0677cf9be7bfd11711066ddde6791f
luna <luna7111@tuta.io>
2025-09-19 21:38:49 +0200

    Feature: Unit tests for ex00

--- mod05/ex00/src/main.cpp
+++ mod05/ex00/src/main.cpp
@@ -1,12 +1,63 @@
-//
-// Created by luna on 19/9/25.
-//
+/*
+ * Name: luna
+ * File: main.cpp
+ * Created on: 19/9/25 15:21
+ */
 
 #include <Bureaucrat.hpp>
 #include <iostream>
+#include <fstream>
 
 int main() {
-    Bureaucrat pablo("Pablo", 10);
 
-    std::cout << pablo << std::endl;
+    try {
+        Bureaucrat pablo("Pablo", 100);
+
+        std::cout << pablo.getName() << std::endl;
+        std::cout << pablo.getGrade() << std::endl;
+
+        std::cout << pablo << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    try {
+        Bureaucrat jasmine("Jasmine", 0);
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    try {
+        Bureaucrat nina("Nina", 2);
+
+        std::cout << nina << std::endl;
+        nina.incrementGrade();
+        std::cout << nina << std::endl;
+        nina.incrementGrade();
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    try {
+        Bureaucrat simba("Simba", 149);
+
+        std::cout << simba << std::endl;
+        simba.decrementGrade();
+        std::cout << simba << std::endl;
+        simba.decrementGrade();
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
 }
