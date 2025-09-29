434c1c1611f45af2ec60594b79931b2621e1c7e3
luna <luna7111@tuta.io>
2025-09-29 16:57:15 +0200

    Fix: Moved macros to auxiliary header file as they are not exclusive to bureaucrats

---
+++ mod05/ex01/include/Auxiliary.hpp
@@ -0,0 +1,13 @@
+/*
+ * Name: luna
+ * File: Aux.hpp
+ * Created on: 28/9/25 17:53
+ */
+
+#ifndef EX01_AUX_HPP
+#define EX01_AUX_HPP
+
+const unsigned int MAXIMUM_GRADE = 1;
+const unsigned int MINIMUM_GRADE = 150;
+
+#endif //EX01_AUX_HPP
