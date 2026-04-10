fdedc43166a969497f881c9632ce40b019101e3c
Luna <luna7111@tuta.io>
2026-04-10 11:49:23 +0200

    Added main script

---
+++ mod09/ex00/src/main.cpp
@@ -0,0 +1,37 @@
+#include "../include/BitcoinExchange.hpp"
+
+int main (int argc, char** argv) {
+
+    if (argc != 2) {
+        std::cerr << "Usage: ./btc <file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input_file;
+
+    input_file.open(argv[1], std::ios::in);
+    if (input_file.is_open() == false) {
+        std::cerr << "Couldn't open file." << std::endl;
+        return 1;
+    }
+
+    BitcoinExchange prize_lookup;
+
+    std::string current_line;
+    while (std::getline(input_file, current_line)) {
+        if (inputLineIsEmpty(current_line)) {
+            continue;
+        }
+        if (inputLineIsValid(current_line) == false) {
+            std::cout << "Invalid line -> " << current_line << std::endl;
+            continue;
+        }
+        std::string date = current_line.substr(0, current_line.find(" | "));
+        char* end;
+        double amount = strtod(current_line.substr(current_line.find(" | ") + 3).c_str(), &end);
+
+        double value = prize_lookup.getValue(amount, date);
+        std::cout << date << " => " << amount << " = " << value << std::endl;
+    }
+
+}
